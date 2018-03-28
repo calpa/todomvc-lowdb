e9cfb7c71f05288ae5fe7f064039113e589dc057
Calpa Liu <calpaliu@gmail.com>
2018-03-28 10:07:40 +0800

    Update README.md and fix bugs

--- README.md
+++ README.md
@@ -4,11 +4,21 @@ This is a database using express.js + lowdb.
 
 Github Repo: https://github.com/calpa/todomvc-lowdb
 
+## Start
+```
+git clone https://github.com/calpa/todomvc-lowdb.git
+cd ./todomvc-lowdb
+npm install
+npm start
+```
+
+Then open `localhost:8081` or you may use `PORT=3000 npm start` to use `localhost:3000`. 
+
 ## Support
-1. Add todo
-2. Modify todo with provided id
-3. Soft Delete todo with provided id
-4. Query todo with get param: i
+1. Add todo via post request to `/todos`, {id: number, name: string}
+2. Modify todo with provided id via post request to `/todos`, {id: number, name: string}
+3. Soft Delete todo with provided id, delete request to `/todos`, {id: number}
+4. Query todo with get param: name to `/todos?name=`, e.g. `/todos?name=apple`
 
 ## Special Thanks
 NetEase
--- index.js
+++ index.js
@@ -10,35 +10,35 @@ app.use(bodyParser.json());
 const adapter = new FileSync('db.json');
 const db = low(adapter);
 
-const port = process.env.port || 8081;
+const port = process.env.PORT || 8081;
 
 db.defaults({ todos: [] })
   .write();
 
 app.get('/', function (req, res) {
-      res.send('Here is the database of todomvc-database');
+    res.send('Here is the database of todomvc-database');
 });
 
 // Get All todos
-app.get('/todos', function(req, res) {
-    const todos = db.get('todos').value();
+app.get('/todos', async function(req, res) {
+    const todos = await db.get('todos').value();
 
     // Query with name
     // i = includes
-    if (req.query) {
+    if (req.query.name) {
+        console.log(`Received with query name: ${req.query.name}`);
         const result = db.get('todos')
             .filter(function(o) {
                 // Return the existing todo that includes the query string
-                return o.name.includes(req.query.i) && !o.isDeleted;
+                const name = String.prototype.toLowerCase.apply(o.name);
+                return name.includes(req.query.name) && !o.isDeleted;
             })
             .value();
 
-        console.log(result);
         res.send(result);
 
         return;
     }
-    console.log(todos);
     res.send(todos);
 });
 
@@ -56,13 +56,13 @@ app.post('/todos', function (req, res) {
             // Rewrite the name of that todo
             db.get('todos')
               .find({ id: todo.id })
-              .assign({ name: todo.name})
+              .assign({ name: todo.name })
               .write();
             console.log('Update item: ' + todo.name + ' id: ' + todo.id);
         } else {
         // Create the item with that id
         db.get('todos')
-            .push({id: todo.id, name: todo.name})
+            .push({id: todo.id, name: todo.name, isDeleted: false})
             .write();
 
         console.log('Successfully added todo: ' + todo.name + ' into the database');
@@ -86,5 +86,6 @@ app.delete('/todos', function(req, res) {
     res.send(db.get('todos'));
 });
 
-app.listen(port);
-console.log(`Listening to ${port}`);
+app.listen(port, () => {
+    console.log(`Listening to ${port}`);
+});
